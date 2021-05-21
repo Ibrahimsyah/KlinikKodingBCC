2ca216e07baf366924676d6a471bf1bec29379d5
Ibrahimsyah Zairussalam <ibra.himsyah10@gmail.com>
2021-05-21 19:48:36 +0700

    abstract + interface

--- pertemuan3/AbstractPlayground.java
+++ pertemuan3/AbstractPlayground.java
@@ -9,6 +9,57 @@ package pertemuan3;
  *
  * @author Ibrahimsyah
  */
+abstract class Book {
+
+    String title;
+
+    public Book(String title) {
+        this.title = title;
+    }
+
+    void baca() {
+        System.out.println("Buku sedang dibaca");
+    }
+
+    abstract void jual();
+}
+
+class Novel extends Book {
+
+    public Novel(String title) {
+        super(title);
+    }
+
+    @Override
+    void jual() {
+        System.out.println("Novel dijual di toko buku");
+    }
+
+}
+
+class Majalah extends Book {
+
+    public Majalah(String title) {
+        super(title);
+    }
+
+    @Override
+    void jual() {
+        System.out.println("Majalah dijual di supermarket");
+    }
+
+}
+
 public class AbstractPlayground {
-    
+
+    public static void main(String[] args) {
+        Book novel = new Novel("Novel1");
+        novel.jual();
+        novel.baca();
+
+        Book majalah = new Majalah("Majalah1");
+        majalah.jual();
+        majalah.baca();
+
+    }
 }
--- pertemuan3/InterfacePlayground.java
+++ pertemuan3/InterfacePlayground.java
@@ -9,6 +9,56 @@ package pertemuan3;
  *
  * @author Ibrahimsyah
  */
+interface Robot {
+
+    void bergerak();
+
+    void tembak();
+}
+
+class Ultraman implements Robot {
+
+    @Override
+    public void bergerak() {
+        System.out.println("Ultraman sedang bergerak");
+    }
+
+    @Override
+    public void tembak() {
+        System.out.println("Ultraman Menembakkkkk!");
+    }
+
+}
+
+class Transformer implements Robot {
+
+    @Override
+    public void bergerak() {
+        System.out.println("Transformer sedang bergerak");
+    }
+
+    @Override
+    public void tembak() {
+        System.out.println("Transformer tidak bisa menembak");
+    }
+
+}
+
 public class InterfacePlayground {
+
+    public static void main(String[] args) {
+        Ultraman ultraman = new Ultraman();
+        ultraman.bergerak();
+        ultraman.tembak();
+        
+        Robot transformer = new Transformer();
+        transformer.bergerak();
+        transformer.tembak();
+        
+        gerakkanRobot(ultraman);
+    }
     
+    static void gerakkanRobot(Robot robot){
+        robot.bergerak();
+    }
 }
